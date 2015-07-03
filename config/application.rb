eea731013abdd96e605c1f18d7c448f88f5ac564
Anurag <anurag.jaiswal@libserv.co>
2015-07-03 16:44:02 +0530

    Changes in CKEDITOR

--- config/application.rb
+++ config/application.rb
@@ -19,11 +19,11 @@ module Website
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+config.assets.precompile += Ckeditor.assets
+config.assets.precompile += %w(ckeditor/*)
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
