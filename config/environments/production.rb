4966c826134a0d969622243c58dde59e5ca20d7d
Anurag <anurag.jaiswal@libserv.co>
2015-07-08 16:04:55 +0530

    changed host to amazon in production.rb mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.default_url_options = {:host => 'http://libserv-web-env-de6n8e3nap.elasticbeanstalk.com', :protocol => 'http'}
+  config.action_mailer.default_url_options = {:host => 'http://libserv-web-env-de6n8e3nap.elasticbeanstalk.com/'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
