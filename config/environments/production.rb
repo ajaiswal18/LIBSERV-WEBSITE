37b10512d477ff9db0bce32b8cd11ea389e11d14
Anurag <anurag.jaiswal@libserv.co>
2015-07-09 11:03:01 +0530

    Changes in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,9 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
